984c28564fae365e66349edc69b62ae7eab73179
Ariel Fogel <fogeltine@gmail.com>
2025-10-17 14:13:36 +0300

    generate single-file docs in generate_md_files.sh

--- scripts/generate_md_files.sh
+++ scripts/generate_md_files.sh
@@ -5,6 +5,14 @@ set -xeuo pipefail
 # navigate to the repository root
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
+# Generate single-file documentation
+cd single-file-document
+python3 concatenate_to_single_file.py
+./clean_markdown_file.sh
+sed -i '1s/^.*$/# DuckLake Documentation/' ducklake-docs-cleaned.md
+cp ducklake-docs-cleaned.md ../ducklake-docs.md
+cd ..
+
 shopt -s extglob
 rm -rf docs/!(stable)
 find . -name '*.md' | xargs -I {} sed -E -i 's|\{% link ([^\}]*) %\}|https://ducklake.select/\1|g' {}
